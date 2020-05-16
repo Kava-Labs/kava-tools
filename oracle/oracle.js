b1a3a41e26327180c2dc49b421e304c61a6a290b
Kevin Davis <kjydavis3@gmail.com>
2020-05-16 13:59:43 -0400

    update to standalone class

--- oracle/oracle.js
+++ oracle/oracle.js
@@ -203,26 +203,5 @@ class PriceOracle {
     }
 }
 
-var main = async () => {
-    // Load environment variables
-    const lcdURL = process.env.LCD_URL;
-    const mnemonic = process.env.MNEMONIC;
-    const marketIDs = process.env.MARKET_IDS.split(",");
-    const expiry = process.env.EXPIRY;
-    const expiryThreshold = process.env.EXPIRY_THRESHOLD;
-    const deviation = process.env.DEVIATION;
-
-    // Initiate price oracle
-    oracle = new PriceOracle(marketIDs, expiry, expiryThreshold, deviation);
-    await oracle.initClient(lcdURL, mnemonic);
-
-    // Start cron job
-    cron.schedule(process.env.CRONTAB, () => {
-        oracle.postPrices()
-    });
-}
-
-main();
-
 
 module.exports.PriceOracle = PriceOracle
